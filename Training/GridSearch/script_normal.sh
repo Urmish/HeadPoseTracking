1ac7d2ae50c99c9d3f249c751a775558e6cbad00
Urmish Thakker <urmish24@gmail.com>
2015-05-07 14:24:10 -0500

    Script for Converting Basic yaml to generating yaml's for grid search

---
+++ Training/GridSearch/script_normal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /home/urmish/Documents/OpenCV/Practice/configs/matches_normal
+array=$(ls "/home/urmish/Documents/OpenCV/Practice/configs/DataFiltersWithNormal/")
+knn=(1 5 10)
+echo ${knn[@]}
+echo $array
+epsilon=(0 4 8)
+for file in ${array}
+do for val1 in ${knn[@]}
+	do for val2 in ${epsilon[@]}
+		do
+			file2=$(echo "${val1}_${val2}_${file}")
+			echo $file2
+			full_file1=$(echo "/home/urmish/Documents/OpenCV/Practice/configs/DataFiltersWithNormal/${file}")
+			cp $full_file1 $file2
+			sed -i "s/knn: 1/knn: $val1/" $file2
+			sed -i "s/epsilon: 0/epsilon: $val2/" $file2
+		done	
+	done
+	
+done
+
